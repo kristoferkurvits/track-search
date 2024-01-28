b6fb872e3406aff95d9a100329216c99e8f3ce7a
Kristofer Kurvits <kurvitskristofer@gmail.com>
2024-01-29 00:01:09 +0200

    mongoDB setup

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/track-search', {
+    await mongoose.connect('mongodb://mongodb:27017/track-search-database', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
